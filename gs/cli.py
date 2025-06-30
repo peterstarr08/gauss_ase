64cc0b570b59dfbfe6bd42a816bce5b8e796ee82
Rubin Singh <singhrubin08@gmail.com>
2025-06-30 22:06:13 +0530

    bug fix

--- gs/cli.py
+++ gs/cli.py
@@ -4,10 +4,10 @@ from gs.calculator import start
 
 def main():
     parser = argparse.ArgumentParser(description="Run DFT SPE + force calculations using Gaussian.")
-    parser.add_argument("input", help="Path to input file with multiple configs")
-    parser.add_argument("output", help="Path to output file for processed configs")
-    parser.add_argument("--method", default="B3LYP", help="DFT method (default: B3LYP)")
-    parser.add_argument("--basis", default="6-31G(d)", help="Basis set (default: 6-31G(d))")
+    parser.add_argument("--input",type=str, required=True, help="Path to input file with multiple configs")
+    parser.add_argument("--output",type=str, required=True, help="Path to output file for processed configs")
+    parser.add_argument("--method",type=str, default="B3LYP", help="DFT method (default: B3LYP)")
+    parser.add_argument("--basis",type=str, default="6-31G(d)", help="Basis set (default: 6-31G(d))")
 
     args = parser.parse_args()
 
